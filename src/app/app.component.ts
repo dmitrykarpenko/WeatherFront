d7cc3eb7256f3f16158dc7f592da61d1da805e75
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-06-21 11:59:18 +0300

    ForecastService added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ForecastService } from './forecasts/forecast.service';
 
 @Component({
   selector: 'wf-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ForecastService],
 })
 export class AppComponent {
   title: string = 'Weather Front';
